0e88e382f18207b2effe17532b3babb771b3f883
cliffordaustin <106864832+cliffordaustin@users.noreply.github.com>
2026-05-23 02:20:44 +0000

    Initial Commit

---
+++ Chopper/ChopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChopperApp.swift
+//  Chopper
+//
+//  Created by Clifford Austin on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct ChopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
